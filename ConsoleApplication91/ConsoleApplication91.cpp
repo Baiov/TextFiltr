91833e048d6b2c1107c7bd2a50a2ef05e3fc0b2b
Ivan <Ivan@DESKTOP-DH3K34P>
2024-12-19 18:52:14 +0300

    старт

--- ConsoleApplication91/ConsoleApplication91.cpp
+++ ConsoleApplication91/ConsoleApplication91.cpp
@@ -1,7 +1,12 @@
 ﻿#include <iostream>
+#include <fstream>
 using namespace std;
 int main()
 {
-   
+	setlocale(0, "rus");
+	ifstream in{ "file.txt" };
+	char mas[1024];
+	in.getline(mas, 1024);
+	cout << mas;
 }
 
